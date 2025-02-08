7bece61f056af564f0072c8aa276fa07d06eda2a
sasha1457 <olexhrapov@gmail.com>
2025-02-08 19:35:08 +0200

    sdd

---
+++ cart.js
@@ -0,0 +1,37 @@
+let cart = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".sectionm h3").forEach((button) => {
+        button.addEventListener("click", () => {
+            let item = button.parentElement;
+            let name = item.querySelector("h1").textContent;
+            let price = parseFloat(item.querySelector("h2").textContent.replace("$", ""));
+            
+            addToCart(name, price);
+        });
+    });
+});
+
+function addToCart(name, price) {
+    cart.push({ name, price });
+    updateCart();
+}
+
+function updateCart() {
+    let cartList = document.getElementById('cart-list');
+    let totalPrice = document.getElementById('total-price');
+
+    cartList.innerHTML = "";
+    let total = 0;
+
+    cart.forEach((item) => {
+        let li = document.createElement("li");
+        li.textContent = `${item.name} - $${item.price.toFixed(2)}`;
+        cartList.appendChild(li);
+        total += item.price;
+    });
+
+    total = Math.round(total * 2) / 2;
+
+    totalPrice.textContent = total.toFixed(2);
+}
